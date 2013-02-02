3ed4c580f8f562a3f202c6b0d6aefc1b2517f87d
Rasmus Graver <rasmus.graver@gmail.com>
2013-02-02 09:55:36 +0100

    New layout with nav floating left. Added images of nico silje

--- index.php
+++ index.php
@@ -7,38 +7,46 @@
         <?php include("shared/head.html"); ?>
     </head>
     <body class="index">
-        <div class="body1">
-            <div class="main">
+
+
                 <?php include("shared/header.php"); ?>
-            </div>
-        </div>
+
+
 
 
         <div class="body3">
             <div class="main whiteback">
                 <!-- content -->
                 <article id="content">
-                    <div class="wrapper">
-                        <div class="pad">
-                            <h2><?php echo WELCOME_HEAD; ?></h2>
-                            <?php echo WELCOME_TEXT; ?>
+                    <div class="text_wrapper">
+                        <div class="wrapper">
+                            <div class="pad">
+                                <h2><?php echo WELCOME_HEAD; ?></h2>
+                                <?php echo WELCOME_TEXT; ?>
+                            </div>
+                        </div>
+
+                        <div class="wrapper">
+                            <div class="pad">
+                                <h2><?php echo TRANSPORT_HEAD; ?></h2>
+                                <h3><?php echo TRANSPORT_PLANE_HEAD; ?></h3>
+                                <ul>
+                                    <li>SAS: <a href="http://www.sas.no">www.SAS.no</a></li>
+                                    <li>Norwegian: <a href="http://www.Norwegian.no">www.Norwegian.no</a></li>
+                                </ul>
+                                <br />
+                                <h3><?php echo TRANSPORT_BUSS_HEAD; ?></h3>
+                                <?php echo TRANSPORT_BUSS_TEXT; ?>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="wrapper">
-                        <div class="pad">
-                            <h2><?php echo TRANSPORT_HEAD; ?></h2>
-                            <h3><?php echo TRANSPORT_PLANE_HEAD; ?></h3>
-                            <ul>
-                                <li>SAS: <a href="http://www.sas.no">www.SAS.no</a></li>
-                                <li>Norwegian: <a href="http://www.Norwegian.no">www.Norwegian.no</a></li>
-                            </ul>
-                            <br />
-                            <h3><?php echo TRANSPORT_BUSS_HEAD; ?></h3>
-                            <?php echo TRANSPORT_BUSS_TEXT; ?>
-                        </div>
+                    <div class="image_wrapper">
+                        <img src="images/nicoSiljeSjo.jpg" alt="" />
+                        <img src="images/nicoSiljeSkogen.jpg" alt="" />
+                        <img src="images/nicoSiljeSnowTopp.jpg" alt="" />
+                        <img src="images/nicoSiljNat.jpg" alt="" />
+                        <img src="images/nicoSiljPar.jpg" alt="" />
                     </div>
-
                 </article>
                 <!-- content / -->
             </div>
--- shared/header.php
+++ shared/header.php
@@ -6,28 +6,29 @@
             <li><a href="<?php echo CURRENT_PAGE; ?>.php?lang=nb" class="flag_heart"><img src="images/nb_heart.jpg" alt="" /></a></li>
             <li><a href="<?php echo CURRENT_PAGE; ?>.php?lang=en" class="flag_heart"><img src="images/uk-usa-heart.jpg" alt="" /></a></li>
         </ul>
-    </div>
-    <div class="wrapper">
-        <div class="floatLeft">
+
+        <div class="upperlogo">
             <h1><a href="index.php" id="logo">Nico og Silje</a></h1>
             <p>Kjerringøy 22. juni 2013</p>
         </div>
-        <nav>
-            <ul id="menu">
-                <li><a href="index.php<?php echo LINK_PARAMS; ?>"<?php
-if (CURRENT_PAGE == 'index') {
-    echo 'class="active"';
-}
-?> >HJEM</a></li>
-                <li><a href="hvor.php<?php echo LINK_PARAMS; ?>"<?php
-if (CURRENT_PAGE == 'hvor') {
-    echo 'class="active"';
-}
-?> >KJERRINGØY</a></li>
-
-            </ul>
-        </nav>
     </div>
 
 </header>
 <!-- / header -->
+
+
+<nav>
+    <ul>
+        <li><a href="index.php<?php echo LINK_PARAMS; ?>"<?php
+if (CURRENT_PAGE == 'index') {
+    echo 'class="active"';
+}
+?> >HJEM</a></li>
+        <li><a href="hvor.php<?php echo LINK_PARAMS; ?>"<?php
+               if (CURRENT_PAGE == 'hvor') {
+                   echo 'class="active"';
+               }
+?> >KJERRINGØY</a></li>
+
+    </ul>
+</nav>
