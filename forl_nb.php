b8b17ff834b735bed2bfc203a151e1e3c5e6cfb9
Rasmus Graver <rasmus.graver@gmail.com>
2013-02-12 22:15:41 +0100

    Added toastmaster to forlovere. And some style fixing

--- forl_nb.php
+++ forl_nb.php
@@ -14,5 +14,22 @@
         <p>
             Rasmus og Sven Nico møttes over klaveret da de var 10 år gamle. Siden har de vært sammen gjennom tykt og tynt, og det er også Rasmus som er datageniet som utgjør den andre halvdelen i det supre firmaet Snappit!
         </p>
+
+        <h2>Toastmaster</h2>
+
+        <h3>Geir Thorvaldsen</h3>
+
+        <p>
+            Geir har fått det ærefulle oppdraget om å lose oss gjennom festaftenen den 22. juni.  Han er en ærlig, myndig og veltalende person med en nydelig røst som er lett å få øye på.
+        </p>
+
+        <p>
+            Geir, bedre kjent som Der Kampfdrinker, er vi sikre på at har den riktige kompetansen til å balansere aftenens ulike poster på en god måte, slik at vi alle får en riktig så hyggelig feststund!
+        </p>
+
+        <p>
+            Ønsker du å bidra med noe i løpet av middagen, så ber vi om at du tar kontakt med Geir innen 1.juni på e-post: Geir@snappit.com
+        </p>
+
     </div>
 </div>
