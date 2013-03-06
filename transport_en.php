c229b828db5739d28d0071d0007a8250f9fa0834
Rasmus Graver <rasmus.graver@gmail.com>
2013-03-06 20:20:18 +0100

    Added more info about transport with nice google links and ferry

--- transport_en.php
+++ transport_en.php
@@ -4,22 +4,30 @@
 
         <h3>By plane to/from Bodø (BOO):</h3>
         <ul>
-            <li>SAS: <a href="http://www.sas.no">www.SAS.no</a></li>
-            <li>Norwegian: <a href="http://www.Norwegian.no">www.Norwegian.no</a></li>
+            <li>SAS: <a href="http://www.sas.no" target="_blank" >www.sas.no</a></li>
+            <li>Norwegian: <a href="http://www.norwegian.no" target="_blank" >www.norwegian.no</a></li>
         </ul>
-        <br />
-        <h3>From the airport to Kjerringøy:</h3>
-        <ol>
-            <li> Rent a car and drive to Kjerringøy.
-                You can follow this map. (Må få inn en aktiv link på ordet this)
-                After approximately 35 min you have to take the ferry. You by your ferry tickets on board. After the ferry you drive for approximately 20 min. Take the road to the left just after the church  (on your right hand side).
-            </li><li> Take the bus
-                You have to get to the bus station down town. This is a 30 min walk, or 5 min taxi, or take the bus. (Leaves aproximately every 15 min)
-                - From the bus station take the <a href="http://www.177nordland.no/?ac_id=1&ac_parent=1" >bus #400.</a>
 
-            </li><li> If non of these solutions suits your travel times, please let us know
+        <h3>From the airport to Kjerringøy:</h3>
+        <ul>
+
+            <li> Rent a car and drive to Kjerringøy.
+                You can follow <a href="http://maps.google.com/maps/ms?ie=UTF&msa=0&msid=210574452578287680724.0004d7466075f2cb70cbf"
+                                  target="_blank" >THIS</a>  map.
+                After approximately 35 min you have to take the ferry. 
+                You by your ferry tickets on board. 
+                After the ferry you drive for approximately 20 min. 
+                Take the road to the left just after the church  (on your right hand side).
+
+              <br />     <a href="images/ferry2.png" target="_blank" >HERE</a> are the times for the FERRY
+
+            </li><li> Take the bus. The bus leaves from the airport at (Friday):
+                <br />   06.55,   07.45, 12.45, 13.50, 15.35, 16.10
+
+            </li><li>  If non of these options work for you, let us know, so we can fix something
+
             </li>
-        </ol>
+        </ul>
 
     </div>
 </div>
