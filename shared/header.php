90136e6a66d679a1c6b1524bf88293f3db0ad7bc
Rasmus Graver <rasmus.graver@gmail.com>
2013-04-17 19:20:35 +0200

    Fix on heart language setters, and removed under dev

--- shared/header.php
+++ shared/header.php
@@ -4,8 +4,8 @@
     <div class="wrapper">
 
         <ul id="icons">
-            <li><a href="<?php echo CURRENT_PAGE; ?>.php?lang=nb" class="flag_heart"><img src="images/nb_heart.jpg" alt="" /></a></li>
-            <li><a href="<?php echo CURRENT_PAGE; ?>.php?lang=en" class="flag_heart"><img src="images/uk-usa-heart.jpg" alt="" /></a></li>
+            <li><a href="<?php echo CURRENT_PAGE; ?>.php?lang=nb" class="flag_heart"><img src="images/nb_heart.jpg" alt="nb" height="40" /></a></li>
+            <li><a href="<?php echo CURRENT_PAGE; ?>.php?lang=en" class="flag_heart"><img src="images/uk-usa-heart.jpg" alt="en" height="40" /></a></li>
         </ul>
 
         <div class="upperlogo">
@@ -13,7 +13,7 @@
             <p>Kjerringøy 22. juni 2013</p>
         </div>
 
-        <h2 style="position: absolute;right: 63px;top: 1px;font-size: 24px;"><?php echo UNDER_DEV; ?></h2>
+        <!-- h2 style="position: absolute;right: 63px;top: 1px;font-size: 24px;" > php echo UNDER_DEV;  </h2 -->
     </div>
 
 </header>
